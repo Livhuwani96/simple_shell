762681144c5c13a12255b59e4f86499a3b81ebfa
livhuwani96 <livhuwani96@gmial.com>
2023-05-25 11:22:24 -0700

    atoi

---
+++ _atoi.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * interactive - will return positive if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: interactive mode 1, and 0 if not true
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - It checks the character if is a delimeter
+ * @c: checks the character
+ * @delim: it checks the delimeter of the string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _isalpha - It will check for the alphabetic of characters
+ * @c: Character is to input
+ * Return: 1 if letter c is alphabetic, 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - It converts a string to an integer
+ * @s: the string will be converted
+ * Return: 0 if there are no numbers in the string
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
+
